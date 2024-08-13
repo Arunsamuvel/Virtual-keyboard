8706da3c3b506bbafcb6561e589d4fdae52f49a8
Arunsamuvel <arun15ee03@gmail.com>
2024-08-13 20:15:25 +0530

    JAVASCRIPT Tested

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const display = document.getElementById("text-display");
+
+const backSpace = document.getElementById("Backspace");
+
+// const textDisplay = display.value;
+
+// function click(input) {
+//   input.value = textDisplay;
+//   console.log("connected");
+// }
+
+const input = [];
+
+function appendToDisplay(input) {
+  display.value += input;
+  console.log("connected");
+}
